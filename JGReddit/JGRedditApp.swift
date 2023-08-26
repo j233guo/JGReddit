96e3fc74db9920e42862d61ee6cac496948c4447
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2023-08-26 17:18:17 -0400

    Initial Commit

---
+++ JGReddit/JGRedditApp.swift
@@ -0,0 +1,17 @@
+//
+//  JGRedditApp.swift
+//  JGReddit
+//
+//  Created by Jiaming Guo on 2023-08-26.
+//
+
+import SwiftUI
+
+@main
+struct JGRedditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
