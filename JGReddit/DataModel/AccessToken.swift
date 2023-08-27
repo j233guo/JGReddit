95eddb6898fac10c980aa5a625e5ea239814dac3
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2023-08-26 21:25:29 -0400

    supported obtaining access token

---
+++ JGReddit/DataModel/AccessToken.swift
@@ -0,0 +1,15 @@
+//
+//  AccessToken.swift
+//  JGReddit
+//
+//  Created by Jiaming Guo on 2023-08-26.
+//
+
+import Foundation
+
+struct AccessToken: Codable {
+    var access_token: String
+    var token_type: String
+    var expires_in: Int
+    var scope: String
+}
---
+++ JGReddit/Network/NetworkManager.swift
@@ -0,0 +1,68 @@
+//
+//  NetworkManager.swift
+//  JGReddit
+//
+//  Created by Jiaming Guo on 2023-08-26.
+//
+
+import Alamofire
+import Foundation
+
+class NetworkManager {
+    private var baseUrl = "https://www.reddit.com"
+    private var clientId: String
+    private var secretKey: String
+    private var username: String
+    private var password: String
+    private var accessToken: String = ""
+    private var header: HTTPHeaders = [
+        "User-Agent": "swift:journey-grid:v0.0.1 (by /u/anonymousaudience)",
+    ]
+    
+    static let shared = NetworkManager()
+    
+    init() {
+        clientId = credential.clientId
+        secretKey = credential.secretKey
+        username = credential.username
+        password = credential.password
+    }
+}
+
+extension NetworkManager {
+    func checkAccessToken() -> Bool {
+        !accessToken.isEmpty
+    }
+    
+    func obtainAccessToken() -> Bool {
+        var success = true
+        let authData = "\(clientId):\(secretKey)".data(using: .utf8)
+        guard let authValue = authData?.base64EncodedString() else {
+            return false
+        }
+        self.header["Authorization"] = "Basic \(authValue)"
+        let parameters: [String: String] = [
+            "grant_type": "password",
+            "username": username,
+            "password": password
+        ]
+        AF.request("\(baseUrl)/api/v1/access_token",
+                   method: .post,
+                   parameters: parameters,
+                   encoding: URLEncoding.default,
+                   headers: self.header
+        ).responseDecodable(of: AccessToken.self) { response in
+            switch response.result {
+            case .success(let token):
+                self.accessToken = token.access_token
+                if self.checkAccessToken() {
+                    self.baseUrl = "https://oauth.reddit.com/"
+                    self.header["Authorization"] = "bearer \(self.accessToken)"
+                }
+            case .failure:
+                success = false
+            }
+        }
+        return success
+    }
+}
